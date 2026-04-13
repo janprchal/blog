2c5f8785ced8313417d8999921e468758c4dd6f3
Honza <jan.prchal.ext@buildingminds.com>
2026-04-13 16:57:33 +0200

    fix navigation & subnav

--- src/data/navData.js
+++ src/data/navData.js
@@ -8,7 +8,44 @@ const navData = [
     name: "Blog",
     href: "/blog/",
     iconName: "mdi:blog-outline",
+    hasChildren: true,
+    children: [
+      {
+        name: "All Posts",
+        href: "/",
+        iconName: "mdi:format-list-bulleted",
+      },
+      {
+        name: "CSS",
+        href: "/blog/category/css/",
+        iconName: "mdi:language-css3",
+      },
+      {
+        name: "JavaScript",
+        href: "/blog/category/js/",
+        iconName: "mdi:language-javascript",
+      },
+    ],
+  },
+  {
+    name: "Projects",
+    href: "/projects/",
+    iconName: "mdi:folder-outline",
+    hasChildren: true,
+    children: [
+      {
+        name: "Project A",
+        href: "/projects/a/",
+        iconName: "mdi:folder-outline",
+      },
+      {
+        name: "Project B",
+        href: "/projects/b/",
+        iconName: "mdi:folder-outline",
+      },
+    ],  
   }
+
 ]
 
 export default navData;
